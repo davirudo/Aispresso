8f09fe3437e46c5fb250d6afc4b40c1b2fd6cb1c
David Diamanta <dantadiamanta@gmail.com>
2023-08-22 11:51:18 +0700

    delete notification

--- app/src/main/java/com/bangkit/aispresso/data/model/notification/NotificationModel.kt
+++
@@ -1,7 +0,0 @@
-package com.bangkit.aispresso.data.model.notification
-
-data class NotificationModel (
-    var title : String = "",
-    var subTitle : String = "",
-    var date : String = "",
-)
